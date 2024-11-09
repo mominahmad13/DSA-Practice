a12275479eec54949b31247efe86cecbc66eb81f
mominahmadrao <178374867+mominahmad13@users.noreply.github.com>
2024-11-09 18:05:55 +0500

    Add JosephusProblem Using Circular Linked List

---
+++ Assignments/Assignt01/JosephusProblemUsingCircularLinkedList.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+   public :
+   int data ;
+   Node *next ;
+
+   Node( int value )
+   {
+      data = value ;
+      next = NULL;
+   }
+};
+
+Node *insertAtEnd( Node *head , int arr [] , int size )
+{
+     Node * last =  NULL ;
+     for ( int  i = 0 ; i < size ; i++ )
+     {
+        if ( head == NULL )
+        {
+            head = new Node (arr[i]);
+            head->next = head ;
+            last = head ;
+        }
+        else
+        {
+            Node * temp = new Node (arr[i]);
+            last->next = temp ;
+            last = last->next ;
+            last->next = head ;            
+        }
+     }
+           return head ;
+}
+
+Node *JosephusProblem ( Node *head  , int m )
+{
+   if ( head -> next = head )
+   {
+       return head ;
+   }    
+
+   Node *prev = head ;
+
+   for ( int i = 0 ; i < m -1 ; i ++ )
+   { 
+       prev = prev->next ;
+   }
+       Node *toDelete = prev->next ;
+       prev->next = toDelete->next ;
+       delete toDelete ;
+       return ( JosephusProblem(prev->next , m));
+}
+
+int main ( )
+{
+   int n , m ;
+   cout << "Enter number of people: \n";
+   cin >> n ;
+   cout << "Enter position of person to killed: \n";
+   cin >> m ;
+   
+   int arr [n] = {0};
+   Node *head = NULL ;
+   head = insertAtEnd( head, arr , n ) ;
+   head = JosephusProblem(head,m);
+   cout << "Person Left: " << head->data + 1 << endl ;
+}
